05d67804875d4cedf6ed26f2430840e1dd9fea0f
TheRealDeveloper <78469040+LBWBDeveloper@users.noreply.github.com>
2022-08-10 01:20:41 +0200

    Edit Config! Settings with Config comes in 3 Weeks

--- ApplySystem/src/trd/apply/Apply.php
+++ ApplySystem/src/trd/apply/Apply.php
@@ -11,11 +11,9 @@ use trd\apply\commands\applycommand;
 class Apply extends PluginBase{
 
     public static Config $applys;
-    public static Config $forms;
 
     protected function onEnable(): void{
         self::$applys = new Config($this->getDataFolder()."applys.yml", 2);
-        self::$forms = new Config($this->getDataFolder()."forms.yml", 2);
         Server::getInstance()->getCommandMap()->register("", new applycommand("apply", "opens the apply menu", "", [], "", "apply.command"));
     }
 }
